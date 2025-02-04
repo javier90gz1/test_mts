679e44a5f003763415e6ebc6a7d5dabe67db93d2
javier90gz1 <javier90gz1@gmail.com>
2025-02-04 13:34:22 +0100

    add traduction and some changes in views of kanban details and category group_users helpdesk

--- mts_helpdesk_ticket/models/help_desk.py
+++ mts_helpdesk_ticket/models/help_desk.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class HelpDesk(models.Model):
@@ -13,9 +14,9 @@ class HelpDesk(models.Model):
     name = fields.Char(string="Ticket Name", required=True, tracking=True)
     client_id = fields.Many2one('res.partner', string="Client Name", tracking=True)
     state = fields.Selection([
-        ('new','New'),
-        ('in_progress','In progress'),
-        ('done','Done')], string="State", default='new', tracking=True)
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('done', 'Done')], string="State", default='new', tracking=True)
     description = fields.Html(string="Description", tracking=True)
     create_date = fields.Datetime(string="Creation Date", default=lambda self: fields.Datetime.now())
     user_assigned_id = fields.Many2one('res.users', string="User Assigned", tracking=True)
@@ -23,3 +24,5 @@ class HelpDesk(models.Model):
     def action_check_done(self):
         self.state = 'done'
 
+    def action_check_in_progress(self):
+        self.state = 'in_progress'
